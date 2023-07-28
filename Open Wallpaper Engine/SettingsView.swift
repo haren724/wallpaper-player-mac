93c1436873aaffe6779644da780413cf7f6e329e
Haren <human01d@icloud.com>
2023-07-29 05:29:59 +0800

    Supplement to SettingsView add a primary ui of performance & general selection

--- Open Wallpaper Engine/SettingsView.swift
+++ Open Wallpaper Engine/SettingsView.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
-enum GSQuality: Codable {
+enum GSPlayback: String, CaseIterable, Identifiable, Codable {
+    var id: Self { self }
+    case keepRunning, mute, pause, stop
+}
+
+enum GSAntiAliasingQuality: String, CaseIterable, Identifiable, Codable {
+    var id: Self { self }
+    case low, medium, high, ultra
+}
+
+enum GSPostProcessingQuality: String, CaseIterable, Identifiable, Codable {
+    var id: Self { self }
+    case low, medium, high, ultra
+}
+
+enum GSTextureResolutionQuality: String, CaseIterable, Identifiable, Codable {
+    var id: Self { self }
     case low, medium, high, ultra
 }
 
 struct GlobalSettings: Codable {
-    var quality = GSQuality.medium
+    var otherApplicationFocused = GSPlayback.keepRunning
+    var otherApplicationMaximized = GSPlayback.keepRunning
+    var otherApplicationFullscreen = GSPlayback.keepRunning
+    var otherApplicationPlayingAudio = GSPlayback.keepRunning
+    var displayAsleep = GSPlayback.keepRunning
+    var laptopOnBattery = GSPlayback.keepRunning
+    
+    var antiAliasing = GSAntiAliasingQuality.medium
+    var fps: Double = 30
 }
 
 class GlobalSettingsViewModel: ObservableObject {
@@ -21,11 +45,12 @@ class GlobalSettingsViewModel: ObservableObject {
             from: UserDefaults.standard.data(forKey: "GlobalSettings") ?? Data()))
     ?? GlobalSettings()
     
-    @Published var selection = 0
+    @Published var selection = 1
     
     func save() {
-        UserDefaults.standard.set(try! JSONEncoder().encode(settings),
-                                  forKey: "GlobalSettings")
+        let data = try! JSONEncoder().encode(settings)
+        print(String(describing: String(data: data, encoding: .utf8)))
+        UserDefaults.standard.set(data, forKey: "GlobalSettings")
     }
 }
 
@@ -97,21 +122,110 @@ struct SettingsView: View {
                     }
                     .frame(width: 200)
                 }
-                
+                HStack {
+                    Text("Display asleep")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Pause").tag(1)
+                        Text("Stop (free memory)").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+                HStack {
+                    Text("Laptop on battery")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Pause").tag(1)
+                        Text("Stop (free memory)").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+                HStack {
+                    Text("Application Rules")
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("Edit").frame(width: 100)
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
             } header: {
                 Label("Playback", systemImage: "play.fill")
             }
             Section {
-                Picker("", selection: $selection) {
-                    Text("Low").tag(0)
-                    Text("Medium").tag(1)
-                    Text("High").tag(2)
-                    Text("Ultra").tag(3)
+                HStack(spacing: 1) {
+                    Button {
+                        
+                    } label: {
+                        Text("Low").frame(maxWidth: .infinity)
+                    }
+                    Divider()
+                    Button {
+                        
+                    } label: {
+                        Text("Medium").frame(maxWidth: .infinity)
+                    }
+                    Divider()
+                    Button {
+                        
+                    } label: {
+                        Text("High").frame(maxWidth: .infinity)
+                    }
+                    Divider()
+                    Button {
+                        
+                    } label: {
+                        Text("Ultra").frame(maxWidth: .infinity)
+                    }
                 }
-                .pickerStyle(.segmented)
-                .padding(.trailing, 6)
-                Text("Spacer")
-                    .frame(height: 1000)
+                .padding(6)
+                .background(Color(nsColor: NSColor.unemphasizedSelectedContentBackgroundColor))
+                .buttonStyle(.borderless)
+                .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                HStack {
+                    Text("Anti-aliasing")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("None").tag(0)
+                        Text("MSAA x2").tag(1)
+                        Text("MSAA x4").tag(2)
+                        Text("MSAA x8").tag(3)
+                    }
+                    .frame(width: 200)
+                }
+                HStack {
+                    Text("Post-precessing")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Disabled").tag(0)
+                        Text("Enabled").tag(1)
+                        Text("Ultra").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+                Picker("Texture Resolution", selection: $selection) {
+                    Text("High Quality").tag(0)
+                    Text("High Performance").tag(1)
+                    Text("Automatic").tag(2)
+                }
+                HStack {
+                    Text("FPS")
+                    Spacer()
+                    Slider(value: $viewModel.settings.fps, in: 10...120)
+                    .frame(width: 200)
+                    Text(String(format: "%.00f", viewModel.settings.fps))
+                        .frame(width: 25)
+                }
+                HStack {
+                    Text("Reflections")
+                    Spacer()
+                    Toggle("Reflection", isOn: .constant(true))
+                        .toggleStyle(.checkbox)
+                        .labelsHidden()
+                }
             } header: {
                 Label("Quality", systemImage: "memorychip.fill")
             }
@@ -120,15 +234,142 @@ struct SettingsView: View {
     }
     
     var general: some View {
-        Text("1")
+        Form {
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Automatic Startup", systemImage: "star.fill")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Basic Setup", systemImage: "gearshape.fill")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("macOS", systemImage: "apple.logo")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Appearance", systemImage: "paintpalette.fill")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Audio", systemImage: "speaker.3.fill")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Video", systemImage: "film")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Advanced", systemImage: "wrench.and.screwdriver.fill")
+            }
+            Section {
+                HStack {
+                    Text("Other Application Focused:")
+                    Spacer()
+                    Picker("", selection: $selection) {
+                        Text("Keep Running").tag(0)
+                        Text("Mute").tag(1)
+                        Text("Pause").tag(2)
+                    }
+                    .frame(width: 200)
+                }
+            } header: {
+                Label("Developer", systemImage: "number")
+            }
+            Section {
+                HStack {
+                    Text("Reset Config")
+                    Spacer()
+                    Button {
+                        viewModel.settings = GlobalSettings()
+                    } label: {
+                        Text("Reset").frame(width: 100)
+                    }
+                    .tint(Color.red)
+                    .buttonStyle(.borderedProminent)
+                }
+            } header: {
+                Label("Reset", systemImage: "exclamationmark.triangle.fill")
+            }
+        }.formStyle(.grouped)
     }
     
     var plugins: some View {
-        Text("2")
-    }
-    
-    var about: some View {
-        Text("3")
+        VStack {
+            Image(systemName: "wrench.and.screwdriver.fill")
+            Text("Working on it")
+        }
     }
     
     var body: some View {
@@ -166,28 +407,6 @@ struct SettingsView: View {
                 }
             }
             .padding(20)
-//            .toolbar {
-//                Button {
-//                    
-//                } label: {
-//                    Label("Perdormance", systemImage: "speedometer")
-//                }
-//                Button {
-//                    
-//                } label: {
-//                    Label("General", systemImage: "gearshape")
-//                }
-//                Button {
-//                    
-//                } label: {
-//                    Label("Plugins", systemImage: "puzzlepiece.extension")
-//                }
-//                Button {
-//                    
-//                } label: {
-//                    Label("About", systemImage: "person.3")
-//                }
-//            }
         }
     }
 }
@@ -195,6 +414,7 @@ struct SettingsView: View {
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+            .environmentObject(GlobalSettingsViewModel())
             .frame(width: 500, height: 600)
     }
 }
