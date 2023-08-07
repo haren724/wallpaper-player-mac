f9069d1db787609e7ba36aa5841a23f457520d6b
Haren <human01d@icloud.com>
2023-08-08 06:14:33 +0800

    Newly Created a WindowController for MainWindow I've noticed the main window's power consumption is a little high due to setting `.releasedWhenClosed` to `false` So this WindowController will take over all about the window's life cycle

---
+++ Open Wallpaper Engine/MainWindowController.swift
@@ -0,0 +1,26 @@
+//
+//  MainWindowController.swift
+//  Open Wallpaper Engine
+//
+//  Created by Haren on 2023/8/8.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController, NSWindowDelegate {
+    override func loadWindow() {
+        
+    }
+    
+    override func windowWillLoad() {
+        
+    }
+    
+    override func windowDidLoad() {
+        
+    }
+    
+    override func windowTitle(forDocumentDisplayName displayName: String) -> String {
+        return "Open Wallpaper Engine"
+    }
+}
