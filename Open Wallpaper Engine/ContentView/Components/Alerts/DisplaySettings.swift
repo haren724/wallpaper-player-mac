8cc28266beabcf098f21b2a9656195cf241d8445
Haren <human01d@icloud.com>
2023-09-04 11:54:03 +0800

    Hide: Display Settings UI since They are not Implemented

--- Open Wallpaper Engine/ContentView/Components/Alerts/DisplaySettings.swift
+++ Open Wallpaper Engine/ContentView/Components/Alerts/DisplaySettings.swift
@@ -24,70 +24,78 @@ struct DisplaySettings: SubviewOfContentView {
                     .bold()
             }
             .buttonStyle(.link)
+            
             Text("Choose Display")
                 .font(.largeTitle)
             
-            VStack(spacing: 5) {
-                Picker(selection: .constant(0)) {
-                    Text("Wallpaper Per Display").tag(0)
-                    Text("").tag(1)
-                } label: { }
-                HStack(spacing: 5) {
-                    Button {
-                        
-                    } label: {
-                        Label("Split", systemImage: "scissors")
-                            .frame(maxWidth: .infinity)
-                    }
-                    Button {
-                        
-                    } label: {
-                        Label("Group", systemImage: "square.on.square.squareshape.controlhandles")
-                            .frame(maxWidth: .infinity)
-                    }
-                }
-                HStack(spacing: 5) {
-                    Button {
-                        
-                    } label: {
-                        Label("Load Profile", systemImage: "doc.text.fill")
-                            .frame(maxWidth: .infinity)
-                    }
-                    Button {
-                        
-                    } label: {
-                        Label("Save Profile", systemImage: "")
-                            .frame(maxWidth: .infinity)
-                    }
-                }
-            }
-            Image("sumeru")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                .overlay {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("2048x2048 MacBook Pro 14''")
-                                .font(.footnote)
-                                .foregroundStyle(Color.white)
-                                .background(Color.secondary)
-                            Spacer()
-                            Image(systemName: "speaker.wave.3.fill")
-                                .padding(5)
-                                .background(Color.secondary)
+            Group {
+                VStack(spacing: 5) {
+                    Picker(selection: .constant(0)) {
+                        Text("Wallpaper Per Display").tag(0)
+                        Text("").tag(1)
+                    } label: { }
+                    HStack(spacing: 5) {
+                        Button {
+                            
+                        } label: {
+                            Label("Split", systemImage: "scissors")
+                                .frame(maxWidth: .infinity)
                         }
-                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            Label("Group", systemImage: "square.on.square.squareshape.controlhandles")
+                                .frame(maxWidth: .infinity)
+                        }
                     }
-                    .padding(3)
-                    .border(Color.accentColor, width: 3)
+                    HStack(spacing: 5) {
+                        Button {
+                            
+                        } label: {
+                            Label("Load Profile", systemImage: "doc.text.fill")
+                                .frame(maxWidth: .infinity)
+                        }
+                        Button {
+                            
+                        } label: {
+                            Label("Save Profile", systemImage: "")
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
                 }
-            HStack {
-                Button("Change Wallpaper") { }
-                    .buttonStyle(.borderedProminent)
-                Button("Remove Wallpaper") { }
+                Image("sumeru")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    .overlay {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("2048x2048 MacBook Pro 14''")
+                                    .font(.footnote)
+                                    .foregroundStyle(Color.white)
+                                    .background(Color.secondary)
+                                Spacer()
+                                Image(systemName: "speaker.wave.3.fill")
+                                    .padding(5)
+                                    .background(Color.secondary)
+                            }
+                            Spacer()
+                        }
+                        .padding(3)
+                        .border(Color.accentColor, width: 3)
+                    }
+                HStack {
+                    Button("Change Wallpaper") { }
+                        .buttonStyle(.borderedProminent)
+                    Button("Remove Wallpaper") { }
+                }
+                Spacer()
             }
-            Spacer()
+            .disabled(true)
+            .blur(radius: 8.0)
+        }
+        .overlay {
+            WorkingInProgress()
         }
     }
 }
