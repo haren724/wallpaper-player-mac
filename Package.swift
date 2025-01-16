1d4ea8959d20f90d63b42c202407a5733a18a29e
haren724 <haren724@icloud.com>
2025-01-16 15:42:53 +0800

    Ready for publishing on github page

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Wallpaper_Player_User_Documentation",
-    products: [.library(name: "Wallpaper_Player_User_Documentation", targets: ["UserDocumentation"])],
-    targets: [.target(name: "UserDocumentation")]
+    products: [
+        .library(
+            name: "Wallpaper_Player_User_Documentation",
+            targets: ["UserDocumentation"])],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")],
+    targets: [
+        .target(name: "UserDocumentation")]
 )
