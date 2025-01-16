8b3c6b21c6e1de6bf2a983412735eadf215846d9
haren724 <haren724@icloud.com>
2025-01-16 13:25:24 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Wallpaper Player User Documentation"
+)
